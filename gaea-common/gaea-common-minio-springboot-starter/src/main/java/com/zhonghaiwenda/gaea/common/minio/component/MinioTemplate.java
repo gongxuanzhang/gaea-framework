b586e4e9d60e5c0a26e1dc90bfd1c74bff140e9c
gxz <514190950@qq.com>
2021-12-16 16:27:59 +0800

    重构minio template

--- gaea-common/gaea-common-minio-springboot-starter/src/main/java/com/zhonghaiwenda/gaea/common/minio/component/MinioTemplate.java
+++ gaea-common/gaea-common-minio-springboot-starter/src/main/java/com/zhonghaiwenda/gaea/common/minio/component/MinioTemplate.java
@@ -1,18 +1,17 @@
 package com.zhonghaiwenda.gaea.common.minio.component;
 
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.MinioClient;
-import io.minio.RemoveBucketArgs;
-import io.minio.RemoveObjectArgs;
-import io.minio.UploadObjectArgs;
+import io.minio.*;
 import io.minio.messages.Bucket;
+import io.minio.messages.DeleteError;
+import io.minio.messages.DeleteObject;
 import io.minio.messages.Item;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.FileAlreadyExistsException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -31,155 +30,6 @@ public class MinioTemplate implements UnsafeOssOperations<MinioClient> {
     public MinioTemplate(MinioClient client) {
         this.client = client;
     }
-
-//
-//
-
-//
-//
-//    private boolean forceDeleteBucket(String bucketName) {
-//        int count = 10;
-//        while (count > 0 && cleanBucket(bucketName)) {
-//            try {
-//                Thread.sleep(new Random().nextInt(30));
-//            } catch (InterruptedException e) {
-//                count--;
-//            }
-//        }
-//        try {
-//            this.client.removeBucket(RemoveBucketArgs.builder().bucket(bucketName).build());
-//            return true;
-//        } catch (Exception e) {
-//            log.error("删除{}桶过程出现问题{}", bucketName, e.getMessage());
-//            return false;
-//        }
-//
-//
-//    }
-//
-//    @Override
-//    public boolean uploadObject(String bucketName, File file) throws FileNotFoundException {
-//        if (!file.exists()) {
-//            throw new FileNotFoundException(file.getAbsolutePath() + "不存在");
-//        }
-//        try {
-//            this.client.uploadObject(
-//                    UploadObjectArgs.builder()
-//                            .bucket(bucketName)
-//                            .object(file.getName())
-//                            .filename(file.getAbsolutePath())
-//                            .build());
-//            return true;
-//        } catch (Exception e) {
-//            log.error("文件{}上传到桶{}失败，{}", file.getName(), bucketName, e.getMessage());
-//            return false;
-//        }
-//
-//    }
-//
-//    @Override
-//    public boolean uploadObject(String bucketName, File file, String rename) throws FileNotFoundException {
-//        if (!file.exists()) {
-//            throw new FileNotFoundException(file.getAbsolutePath() + "不存在");
-//        }
-//        try {
-//            this.client.uploadObject(
-//                    UploadObjectArgs.builder()
-//                            .bucket(bucketName)
-//                            .object(rename)
-//                            .filename(file.getAbsolutePath())
-//                            .build());
-//            return true;
-//        } catch (Exception e) {
-//            log.error("桶{}上传文件出现问题{}", bucketName, e.getMessage());
-//            return false;
-//        }
-//    }
-//
-//
-//
-//
-//    @Override
-//    public File downloadObject(String bucketName, String objectName, String path) throws FileAlreadyExistsException {
-//        File file = new File(path);
-//        if (file.exists()) {
-//            throw new FileAlreadyExistsException(file.getAbsolutePath() + "已经存在");
-//        }
-//        DownloadObjectArgs download = DownloadObjectArgs.builder()
-//                .bucket(bucketName)
-//                .object(objectName)
-//                .filename(path)
-//                .build();
-//        try {
-//            this.client.downloadObject(download);
-//            return file;
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return null;
-//        }
-//    }
-//
-//
-//    @Override
-//    public List<Item> listBucket(String bucketName) {
-//        ListObjectsArgs args = ListObjectsArgs.builder().bucket(bucketName).build();
-//        Iterable<Result<Item>> results = this.client.listObjects(args);
-//        List<Item> result = new ArrayList<>();
-//        for (Result<Item> item : results) {
-//            try {
-//                result.add(item.get());
-//            } catch (Exception e) {
-//                log.error("桶{},获取对象出现问题{}", bucketName, e.getMessage());
-//            }
-//
-//        }
-//        return result;
-//    }
-//
-//    @Override
-//    public boolean cleanBucket(String bucketName) {
-//        List<Item> items = listBucket(bucketName);
-//        if (CollectionUtils.isEmpty(items)) {
-//            return deleteBucketIfEmpty(bucketName);
-//        }
-//
-//        List<DeleteObject> deleteObjectList = new ArrayList<>();
-//        for (Item item : items) {
-//            deleteObjectList.add(new DeleteObject(item.objectName()));
-//        }
-//        RemoveObjectsArgs removeObjectsArgs =
-//                RemoveObjectsArgs.builder().bucket(bucketName).objects(deleteObjectList).build();
-//        Iterable<Result<DeleteError>> result = this.client.removeObjects(removeObjectsArgs);
-//        boolean success = true;
-//        for (Result<DeleteError> deleteErrorResult : result) {
-//            try {
-//                deleteErrorResult.get();
-//            } catch (Exception e) {
-//                log.error("桶{},删除对象出现问题{}", bucketName, e.getMessage());
-//                success = false;
-//            }
-//        }
-//        if (success) {
-//            return deleteBucketIfEmpty(bucketName);
-//        }
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean deleteBucketIfEmpty(String bucketName) {
-//        if (bucketIsExists(bucketName)) {
-//            try {
-//                RemoveBucketArgs remove = RemoveBucketArgs.builder().bucket(bucketName).build();
-//                this.client.removeBucket(remove);
-//                return true;
-//            } catch (Exception e) {
-//                log.error("桶{}删除出现问题{}", bucketName, e.getMessage());
-//                return false;
-//            }
-//        }
-//        RemoveBucketArgs remove = RemoveBucketArgs.builder().bucket(bucketName).build();
-//        return true;
-//    }
 
     @Override
     public boolean createBucket(String bucketName) {
@@ -262,34 +112,79 @@ public class MinioTemplate implements UnsafeOssOperations<MinioClient> {
 
     @Override
     public boolean deleteBucketForce(String bucketName) {
-        return false;
+        if (this.bucketIsExists(bucketName)) {
+            try {
+                RemoveBucketArgs remove = RemoveBucketArgs.builder().bucket(bucketName).build();
+                this.client.removeBucket(remove);
+                return true;
+            } catch (Exception e) {
+                if (MinioExceptionSupport.isBucketNotEmpty(e)) {
+                    //  因为桶不为空所以不能删除
+                    List<Item> items = listBucket(bucketName);
+                    if (CollectionUtils.isEmpty(items)) {
+                        //  如果此时拿到空内容，再次尝试删除桶
+                        return deleteBucket(bucketName);
+                    }
+                    //  尝试清空桶，如果成功 则删除
+                    if (cleanBucket(bucketName)) {
+                        return deleteBucket(bucketName);
+                    }
+                    //  清空桶失败，则直接返回false
+                    log.error("清空桶{}失败", bucketName);
+                    return false;
+                } else {
+                    log.error("桶{}删除出现问题{}", bucketName, e.getMessage());
+                }
+                return false;
+            }
+        }
+        return true;
     }
 
-    //    @Override
-//    public boolean deleteBucket(String bucketName, boolean force) {
-//        if (bucketIsExists(bucketName)) {
-//            if (force) {
-//                return forceDeleteBucket(bucketName);
-//            }
-//            return deleteBucketIfEmpty(bucketName);
-//        }
-//        deleteBucketIfEmpty(bucketName);
-//        return true;
-//    }
+
 
     @Override
     public List<Bucket> findAllBucket() {
-        return null;
+        try {
+            return this.client.listBuckets();
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
     }
 
     @Override
     public List<Item> listBucket(String bucketName) {
-        return null;
+        ListObjectsArgs args = ListObjectsArgs.builder().bucket(bucketName).build();
+        Iterable<Result<Item>> results = this.client.listObjects(args);
+        List<Item> result = new ArrayList<>();
+        for (Result<Item> item : results) {
+            try {
+                result.add(item.get());
+            } catch (Exception e) {
+                log.error("桶{},获取对象出现问题{}", bucketName, e.getMessage());
+            }
+        }
+        return result;
     }
 
     @Override
     public File downloadObject(String bucketName, String objectName, String path) throws FileAlreadyExistsException {
-        return null;
+        File file = new File(path);
+        if (file.exists()) {
+            throw new FileAlreadyExistsException(file.getAbsolutePath() + "已经存在");
+        }
+        DownloadObjectArgs download = DownloadObjectArgs.builder()
+                .bucket(bucketName)
+                .object(objectName)
+                .filename(path)
+                .build();
+        try {
+            this.client.downloadObject(download);
+            return file;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
@@ -305,7 +200,28 @@ public class MinioTemplate implements UnsafeOssOperations<MinioClient> {
 
     @Override
     public boolean cleanBucket(String bucketName) {
-        return false;
+        //  可能其他线程清空了
+        List<Item> items = listBucket(bucketName);
+        if (CollectionUtils.isEmpty(items)) {
+            return true;
+        }
+
+        List<DeleteObject> deleteObjectList = new ArrayList<>();
+        for (Item item : items) {
+            deleteObjectList.add(new DeleteObject(item.objectName()));
+        }
+        RemoveObjectsArgs roa = RemoveObjectsArgs.builder().bucket(bucketName).objects(deleteObjectList).build();
+        Iterable<Result<DeleteError>> result = this.client.removeObjects(roa);
+        boolean success = true;
+        for (Result<DeleteError> deleteErrorResult : result) {
+            try {
+                deleteErrorResult.get();
+            } catch (Exception e) {
+                log.error("桶{},删除对象出现问题{}", bucketName, e.getMessage());
+                success = false;
+            }
+        }
+        return success;
     }
 
     @Override
--- gaea-test/src/test/java/com/zhonghaiwenda/gaea/test/TestApplicationTest.java
+++ gaea-test/src/test/java/com/zhonghaiwenda/gaea/test/TestApplicationTest.java
@@ -1,13 +1,17 @@
 package com.zhonghaiwenda.gaea.test;
 
 import com.zhonghaiwenda.gaea.common.minio.component.MinioTemplate;
+import io.minio.messages.Bucket;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,14 +22,6 @@ class TestApplicationTest {
     @Autowired
     private MinioTemplate template;
 
-    @Test
-    public void addFile() throws FileNotFoundException {
-       template.uploadObject("qujing",new File("/Users/gongxuanzhang/Desktop/tincery_pro.cer.json"));
-    }
-    @Test
-    public void deleteBucket() throws FileNotFoundException {
-       template.deleteBucket("asdf");
-    }
 
     @Test
     @DisplayName("在不存在的桶中上传文件")
@@ -53,6 +49,32 @@ class TestApplicationTest {
     @DisplayName("删除一个存在的桶，但是有内容")
     public void deleteNotEmptyBucket(){
         template.deleteBucket("suibian");
+    }
+
+    @Test
+    @DisplayName("强制删除有内容桶")
+    public void forceDeleteBucket(){
+        template.deleteBucketForce("suibian");
+    }
+
+
+    @Test
+    @DisplayName("上传不存在的文件")
+    public void uploadNotExistsFile() {
+        Assertions.assertThrows(FileNotFoundException.class, () -> {
+            template.uploadObject("aaa",new File("asdf"),"ff");
+        });
+
+    }
+
+
+    @Test
+    @DisplayName("查看所有桶")
+    public void findAllBucket(){
+        List<Bucket> allBucket = template.findAllBucket();
+        for (Bucket bucket : allBucket) {
+            System.out.println(bucket.name());
+        }
     }
 
 
